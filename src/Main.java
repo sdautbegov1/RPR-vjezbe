d252c89b3197fc871919fd2701f2437c22039cb5
saradaut <136235271+saradaut@users.noreply.github.com>
2023-10-24 20:39:49 +0200

    lv1-z2

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        Scanner ulaz = new Scanner(System.in); // Ulazni objekat
+        System.out.println("Unesite broj: ");
+        a = ulaz.nextInt();
+        for(int i = 1; i < a; i++){
+           if(i % sumaCifara(i) == 0) System.out.println(i);
+        }
+    }
+    private static int sumaCifara(int n){
+        int suma = 0;
+        while(n != 0){
+            suma += n%10;
+            n/=10;
+        }
+        return suma;
+    }
+}
